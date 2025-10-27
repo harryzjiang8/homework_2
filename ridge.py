cb7b823e0648219e227b388a5119048b0c3cdd62
harryzjiang8 <harryzjiang@gmail.com>
2025-10-27 14:10:12 -0700

    finish sgd and applications part

--- ridge.py
+++ ridge.py
@@ -3,7 +3,10 @@ from typing import Optional
 
 
 class LinearSolver:
-    """your docstring here"""
+    """
+    Matrix A of shape (m,d)
+    y vector shape (m,)
+    """
 
     def __init__(self, A: np.ndarray, y: np.ndarray) -> None:
         # check dimensions of A
@@ -21,13 +24,54 @@ class LinearSolver:
         return None
 
     def rls(self, reg: Optional[float] = 1.0) -> np.ndarray:
-        """your docstring here"""
-        return NotImplemented
+        """
+        Return coefficients of vector x as np.ndarry of shape (d,)
 
+        :returns solved unregularized least-squares problem if reg is 0 or None
+        :returns solved regularized least-squares problem if reg > 0
+        :raises ValueError: if reg is negative
+        """
+        ATA = self.A.T@self.A
+        ATy = self.A.T@self.y
+        if reg == 0 or reg == None:
+            return np.linalg.solve(ATA, ATy)
+        
+        if reg < 0:
+            raise ValueError("Regularization parameter should be nonnegative or None.")
+
+        if reg > 0:
+            return np.linalg.solve(ATA + reg*np.identity(self.A.shape[1]), ATy)
+
+    
     def sgd(self,
             reg: float = 1.0,
             max_iter: int = 100,
             batch_size: int = 2,
             step_size: float = 1e-2) -> np.ndarray:
-        """your docstring here"""
-        return NotImplemented
+        """Stochastic Gradient Descent
+
+        raises ValueError: if reg is negative
+        """
+
+        if reg < 0:
+            raise ValueError("Regularization parameter should be nonnegative or None.")
+        
+        m, d = self.A.shape
+        x = np.zeros(d)
+        b = np.random.choice(m, batch_size)
+        A_b = self.A[b, :]
+        y_b = self.y[b]
+
+        for i in range(max_iter):
+            x = x - step_size * (2*(A_b.T) @ (A_b@x - y_b) + 2*reg*x)
+            
+        
+        
+        return x
+
+
+
+
+
+
+
